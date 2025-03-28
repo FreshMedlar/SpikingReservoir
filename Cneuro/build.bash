5d31adc70e70b3008294ed291366b773f8b9a832
FreshMedlar <emaxtiger3000@gmail.com>
2025-03-28 11:32:14 +0100

    Setup adjust

--- Cneuro/build.bash
+++ Cneuro/build.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set build directory
-BUILD_DIR="/home/medlar/SpikingReservoir/build"
+BUILD_DIR="/home/medlar/SpikingReservoir/Cneuro/build"
 
 # Create build directory if it doesn't exist
 mkdir -p $BUILD_DIR
@@ -10,7 +10,7 @@ mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
 # Run CMake configuration
-cmake .
+cmake ..
 
 # Compile the project
 make -j$(nproc)
